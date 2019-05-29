077be5accf33535c63fb688ecd434b033cb9146f
Gibran77 <gibranbareto77@gmail.com>
2019-05-29 14:09:58 -0500

    2

--- js/main.js
+++ js/main.js
@@ -1,37 +1,37 @@
-var audio
+var audio;
 
 //Hide Pause Initially
 $('#pause').hide();
-
-//Initializer - Play Fisrt Song 
-initAudio($('#playlist li:frst-child'));
-
-function initAudio(element){
-	var song=element.attr('song');
-	var title=elment.text();
-	var cover=elment.attr('cover');
-	var artist=element.attr('artist');
 	
+//Initializer - Play First Song
+initAudio($('#playlist li:first-child'));
+	
+function initAudio(element){
+	var song = element.attr('song');
+    var title = element.text();
+    var cover = element.attr('cover');
+    var artist = element.attr('artist');
+
 	//Create a New Audio Object
-	audio=new Audio('media/' + song);
+	audio = new Audio('media/' + song);
 	
 	if(!audio.currentTime){
 		$('#duration').html('0.00');
 	}
-	
-	$('#audio-player.title').text(title);
-	$('#audio-plyer.artist').text(artist);
+
+	$('#audio-player .title').text(title);
+    $('#audio-player .artist').text(artist);
 	
 	//Insert Cover Image
 	$('img.cover').attr('src','images/covers/' + cover);
 	
 	$('#playlist li').removeClass('active');
-	element.addClass('active');
+    element.addClass('active');
 }
 
 
 //Play Button
-$('#play').click(function (){
+$('#play').click(function(){
 	audio.play();
 	$('#play').hide();
 	$('#pause').show();
@@ -45,20 +45,44 @@ $('#pause').click(function(){
 	$('#pause').hide();
 	$('#play').show();
 });
-
+	
 //Stop Button
-$('#top').click(function(){
-	audio.pause();
-	audio.currentTime=0;
+$('#stop').click(function(){
+	audio.pause();		
+	audio.currentTime = 0;
 	$('#pause').hide();
 	$('#play').show();
 	$('#duration').fadeOut(400);
 });
 
+//Next Button
+$('#next').click(function(){
+    audio.pause();
+    var next = $('#playlist li.active').next();
+    if (next.length == 0) {
+        next = $('#playlist li:first-child');
+    }
+    initAudio(next);
+	audio.play();
+	showDuration();
+});
+
+//Prev Button
+$('#prev').click(function(){
+    audio.pause();
+    var prev = $('#playlist li.active').prev();
+    if (prev.length == 0) {
+        prev = $('#playlist li:last-child');
+    }
+    initAudio(prev);
+	audio.play();
+	showDuration();
+});
+
 //Playlist Song Click
-$('#playlist li').click(function(){
-	audio.pause();
-	initAudio($(this));
+$('#playlist li').click(function () {
+    audio.pause();
+    initAudio($(this));
 	$('#play').hide();
 	$('#pause').show();
 	$('#duration').fadeIn(400);
@@ -66,25 +90,25 @@ $('#playlist li').click(function(){
 	showDuration();
 });
 
-//Volmen Control
-$('#volumen').change(function(){
-	audio.volumen=parseFloat(this.value/10);
+//Volume Control
+$('#volume').change(function(){
+	audio.volume = parseFloat(this.value / 10);
 });
-
+	
 //Time Duration
 function showDuration(){
-	$(audio).bind('timeupdate',function(){
-		//Get hours and minutes 
-		var s=parseInt(audio.currentTime%60);
-		var m=parseInt((audio.currentTime /60)%60);
+	$(audio).bind('timeupdate', function(){
+		//Get hours and minutes
+		var s = parseInt(audio.currentTime % 60);
+		var m = parseInt((audio.currentTime / 60) % 60);
 		//Add 0 if seconds less than 10
-		if(s < 10){
-			s='0'+ s;
+		if (s < 10) {
+			s = '0' + s;
 		}
-		$('#duration').html(m + '.' + s);
-		var value =0;
-		if(audio.currentTime > 0){
-			valuie = Math.floor((100/audio.duration)*audio.currentTime);
+		$('#duration').html(m + '.' + s);	
+		var value = 0;
+		if (audio.currentTime > 0) {
+			value = Math.floor((100 / audio.duration) * audio.currentTime);
 		}
 		$('#progress').css('width',value+'%');
 	});
